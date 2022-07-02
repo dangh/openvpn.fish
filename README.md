45e2b72ef18c17ee03464e7a7b2295afbc2b5dc4
Dang <huynhminhdang@gmail.com>
2022-07-02 14:02:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# openvpn.fish
+Launch OpenVPN and emit events
+
+### Installation
+
+```sh
+brew install fswatch openvpn
+fisher install dangh/openvpn.fish
+```
---
+++ conf.d/openvpn.fish
@@ -0,0 +1,48 @@
+set -g openvpn_events \
+  'up' \
+  'tls-verify' \
+  'ipchange' \
+  'route-up' \
+  'route-pre-down' \
+  'down'
+
+function openvpn-event-watcher
+  set -U openvpn_watcher_pid $fish_pid
+  command fswatch -0 $argv | while read -z file
+    set -l event
+    string match -q -r '/openvpn-(?<event>\w+)[^/]+$' $file
+    set -l event_argv (cat $file | string collect)
+    set -l var (string replace -a - _ openvpn-event-$event)
+    set -U $var $event_argv
+  end
+end
+
+for event in $openvpn_events
+  set -l var (string replace -a - _ openvpn-event-$event)
+  function openvpn-event-emitter-$event -v $var
+    set -l event (status function | string replace openvpn-event-emitter- '')
+    set -l var (string replace -a - _ openvpn-event-$event)
+    emit openvpn-$event $$var
+  end
+end
+
+function openvpn-event-debugger -a action
+  for event in $openvpn_events
+    set -l funcname (status function)-$event
+    set -l var (string replace -a - _ openvpn-event-$event)
+    functions -e $funcname
+    if test "$action" != 'stop'
+      if set -q $var
+        echo previous event: $event
+        set -S $var
+        echo
+      end
+      function $funcname -e openvpn-$event
+        set -l event (status function | string replace openvpn-event-debugger- '')
+        echo openvpn event: $event
+        set -S argv
+        echo
+      end
+    end
+  end
+end
