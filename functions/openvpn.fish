f120b07a5c001e716cdcdaec6a1673f9bd8a9f2a
Dang <huynhminhdang@gmail.com>
2024-03-14 08:09:20 +0800

    format code with fish_indent

--- functions/openvpn.fish
+++ functions/openvpn.fish
@@ -1,18 +1,18 @@
 function openvpn
-  set -l openvpn_cmd sudo command openvpn $argv --script-security 2
-  set -l files
-  for event in $openvpn_events
-    set -l file (mktemp -t openvpn-$event)
-    set -a files $file
-    set -l cmd echo \$\* \> $file
-    set -a openvpn_cmd --$event (which sh)" --noprofile --norc -c '$cmd'"
-  end
-  nohup fish --private --command "openvpn-event-watcher $files" >/dev/null 2>&1 &
-  function openvpn-cleanup -e openvpn-down
-    # kill watcher and fswatch process
-    kill -9 -$openvpn_watcher_pid
-    # delete event files
-    rm -f $files 2>/dev/null
-  end
-  $openvpn_cmd
+    set -l openvpn_cmd sudo command openvpn $argv --script-security 2
+    set -l files
+    for event in $openvpn_events
+        set -l file (mktemp -t openvpn-$event)
+        set -a files $file
+        set -l cmd echo \$\* \> $file
+        set -a openvpn_cmd --$event (which sh)" --noprofile --norc -c '$cmd'"
+    end
+    nohup fish --private --command "openvpn-event-watcher $files" >/dev/null 2>&1 &
+    function openvpn-cleanup -e openvpn-down
+        # kill watcher and fswatch process
+        kill -9 -$openvpn_watcher_pid
+        # delete event files
+        rm -f $files 2>/dev/null
+    end
+    $openvpn_cmd
 end
